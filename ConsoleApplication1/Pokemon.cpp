288b2f5015d9882a2ae2959bdf42672ba2b396d2
mynamejason <ykim4141@gmail.com>
2020-07-19 15:21:48 -0400

    DataReader
    Completed DataReader.cpp/hpp

---
+++ ConsoleApplication1/Pokemon.cpp
@@ -0,0 +1,34 @@
+#include "Pokemon.hpp"
+
+
+Pokemon::Pokemon() {
+
+}
+
+Pokemon::~Pokemon() {
+
+}
+
+Pokemon::Pokemon(const std::string& name, const std::string& height, const std::string &weight, const std::string& type) {
+
+}
+
+void Pokemon::addFriend(const std::string &name) {
+	
+}
+
+void Pokemon::removeFriendByName(const std::string& name) {
+
+}
+
+void Pokemon::clearFriends() {
+
+}
+
+int Pokemon::numberOfFriends() {
+	return 0;
+}
+
+void Pokemon::info() {
+
+}
