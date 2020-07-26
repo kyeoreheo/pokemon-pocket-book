c847930f0f5f38d1a96fc0939634039dd8f05184
mynamejason <ykim4141@gmail.com>
2020-07-26 14:33:33 -0400

    Pokemon Constructor
    Created Pokemon constructor + more

--- ConsoleApplication1/Pokemon.cpp
+++ ConsoleApplication1/Pokemon.cpp
@@ -1,5 +1,6 @@
 #include "Pokemon.hpp"
-
+#include "DataReader.hpp"
+#include <iostream>
 
 Pokemon::Pokemon() {
 	name_ = "";
@@ -14,31 +15,65 @@ Pokemon::~Pokemon() {
 	weight_ = "";
 	type_ = "";
 	friends_.clear();
+	friendNames_.clear();
 }
 
-Pokemon::Pokemon(const std::string& name, const std::string& height, const std::string &weight, const std::string& type) {
+Pokemon::Pokemon(const std::string& name, const std::string& height, const std::string &weight, const std::string& type, const std::string &friendNames) {
 	name_ = name;
 	height_ = height;
 	weight_ = weight;
 	type_ = type;
+	separateFriends(friendNames);
 }
 
-void Pokemon::addFriend(const std::string &name) {
-	
+
+std::string Pokemon::getName() {
+	return name_;
 }
 
-void Pokemon::removeFriendByName(const std::string& name) {
+void Pokemon::addFriend(const Pokemon &inputFriend) {
+	friends_.push_back(inputFriend);
+}
 
+void Pokemon::removeFriendByName(const std::string &name) {
+	for (int i = 0; i < friends_.size(); i++) {
+		if (friends_[i].getName() == name) { //check string
+			//remove friend
+			friends_.erase(friends_.begin() + i);
+		}
+	}
 }
 
 void Pokemon::clearFriends() {
-
+	friends_.clear();
 }
 
 int Pokemon::numberOfFriends() {
-	return 0;
+	return friends_.size();
 }
 
 void Pokemon::info() {
+	std::cout << "Name: " << name_ << "\n" << "Height: " << height_ << "\n" << "Weight: " << weight_ << "\n" << "Type: " << type_ << std::endl;
+	std::cout << "Friends: ";
+	for (int i = 0; i < friendNames_.size(); i++) {
+		std::cout << friendNames_[i];
+	}
+}
 
+void Pokemon::separateFriends(const std::string& friendName) {
+	std::string temp = "";
+	int currentIndex = 0;
+	std::cout << friendName << std::endl;
+	while (currentIndex < friendName.size()) {
+		std::cout << currentIndex << std::endl;
+		if (friendName[currentIndex] != ',') {
+			temp += friendName[currentIndex];
+			currentIndex++;
+		} else {
+			friendNames_.push_back(temp);
+			temp = "";
+			currentIndex += 1;			
+		}
+	}
+	friendNames_.push_back(temp);
 }
--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -7,14 +7,34 @@
 
 using namespace std;
 
+void generateFriends(const vector<Pokemon> &inputPokeDex) {
+	for(int i = 0; i < inputPokeDex.size(); i++){
+		for (int j = 0; j < inputPokeDex[i].; j++) {
+
+		}
+}
+
 int main()
 {
 	DataReader dataReader("pokemons.txt");
-	cout << dataReader.pokemonNames[3] << endl;
-	cout << dataReader.type[2] << endl;
-	cout << dataReader.height[5] << endl;
-	cout << dataReader.friends[1] << endl;
+	vector<Pokemon> pokeDex;
 
-	Pokemon pokemon(dataReader.pokemonNames[0], dataReader.height[0], dataReader.weight[0], dataReader.type[0]);
+	for (int i = 0; i < dataReader.pokemonNames.size(); i++) {
+		Pokemon temp(dataReader.pokemonNames[i], dataReader.height[i], dataReader.weight[i], dataReader.type[i], dataReader.friends[i]);
+	}
 
+
+	
+	//cout << dataReader.pokemonNames[3] << endl;
+	//cout << dataReader.type[2] << endl;
+	//cout << dataReader.height[5] << endl;
+	//cout << dataReader.friends[1] << endl;
+	//
+	//Pokemon abra(dataReader.pokemonNames[0], dataReader.height[0], dataReader.weight[0], dataReader.type[0], dataReader.friends[0]);
+	//abra.info();
+
+	//Pokemon arbok(dataReader.pokemonNames[2], dataReader.height[2], dataReader.weight[2], dataReader.type[2]);
+	//abra.addFriend(arbok);
+	//abra.info();
+	//cout << dataReader.friends[0];
 }
